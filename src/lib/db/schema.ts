786e4aff00e384688f3d1a185170fd447e4493bd
Cody Milliron <codymilliron.dev@gmail.com>
2025-12-12 12:50:13 -0500

    add schema for feeds

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,11 +1,23 @@
 import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom().notNull(),
+const timestamps = {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at")
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
+};
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  ...timestamps,
   name: text("name").notNull().unique(),
+});
+
+export const feeds = pgTable("feeds", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  name: text("name").notNull().unique(),
+  url: text("url").notNull().unique(),
+  user_id: uuid("user_id").references(() => users.id, { onDelete: "cascade" }),
+  ...timestamps,
 });
