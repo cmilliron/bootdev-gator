b147a0cab7ba6f201e7900824c8e5b30f7d37af3
Cody Milliron <codymilliron.dev@gmail.com>
2026-03-15 23:32:57 -0400

    create queries to create a feedFollow and recieve that a feed with all connected data

--- src/lib/db/queries/feed.ts
+++ src/lib/db/queries/feed.ts
@@ -1,5 +1,13 @@
 import { readConfig } from "src/config";
-import { Feed, feeds } from "../schema";
+import {
+  Feed,
+  feeds,
+  User,
+  users,
+  feedFollows,
+  FollowFeedForPrint,
+  FeedFollow,
+} from "../schema";
 import { eq, and } from "drizzle-orm";
 import { db } from "..";
 
@@ -21,7 +29,40 @@ export async function getFeed(feedID: string, userID: string) {
   return result;
 }
 
+export async function getFeedByUrl(url: string) {
+  const [feed]: Feed[] = await db
+    .select()
+    .from(feeds)
+    .where(eq(feeds.url, url));
+  return feed;
+}
+
 export async function getAllFeeds() {
   const result = await db.select().from(feeds);
   return result;
+}
+
+export async function createFeedFollow(feedId: string, userId: string) {
+  // TODO  It should insert a feed follow record,
+  // but then return all the fields from the feed follow as well as the names of the linked user
+  // and feed.
+  const results = await db
+    .insert(feedFollows)
+    .values({ feed_id: feedId, user_id: userId })
+    .returning();
+  //   console.log("results\n", result);
+  const newFeedFollow = results[0];
+  return newFeedFollow;
+}
+
+export async function getSingleFeedFollowWithData(feedFollowId: string) {
+  const [feedFollowData] = await db
+    .select()
+    .from(feedFollows)
+    .where(eq(feedFollows.id, feedFollowId))
+    .innerJoin(feeds, eq(feedFollows.feed_id, feeds.id))
+    .innerJoin(users, eq(feedFollows.user_id, users.id));
+  // Need to finish
+  console.log(feedFollowData);
+  return feedFollowData;
 }
